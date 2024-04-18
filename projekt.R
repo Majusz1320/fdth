e7f61c5bba08e100a53014837b00d86b18c1b203
Majusz1320 <marekskrzynski3@gmail.com>
2024-04-18 21:12:41 +0200

    te

--- projekt.R
+++ projekt.R
@@ -15,12 +15,14 @@ ui <- fluidPage(
         numericInput('window_by', 'Width of the window slide.', 10, min = 1, max = 10000),
         tabsetPanel(type="tabs",
                   tabPanel("firstsubpage"),
-                  tabPanel("secondsubPanel")),
+                  
+                  tabPanel("secondsubPanel",
+                           downloadButton('download_histogram', 'Download png plot'),
+                           numericInput('width_hist', 'Plot width [cm]', 20, min = 5, max = 25),
+                           numericInput('height_hist', 'Plot height [cm]', 14, min = 5, max = 25),
+                           numericInput('res_hist', 'Resolution', 200, min = 100, max = 500)))),
         
-        downloadButton('download_histogram', 'Download png plot'),
-        numericInput('width_hist', 'Plot width [cm]', 20, min = 5, max = 25),
-        numericInput('height_hist', 'Plot height [cm]', 14, min = 5, max = 25),
-        numericInput('res_hist', 'Resolution', 200, min = 100, max = 500)),
+       
         
     #druga strona ustawień, może miejsce do wyboru danych zaimplementwanych
         tabPanel("secondpage",
