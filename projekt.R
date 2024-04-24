267426a85dfe7f577b155b3a4bd58df54de5513c
Majusz1320 <marekskrzynski3@gmail.com>
2024-04-24 13:55:22 +0200

    24.04

--- projekt.R
+++ projekt.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(ggplot2)
 source("functions.R") 
+library(gggenes)
 
 ui <- fluidPage(
   titlePanel("NGS of Streptomyces"),
@@ -10,13 +11,17 @@ ui <- fluidPage(
                              
     #pierwsza strona dla ładowanych własnych danych              
         tabPanel("firstpage",
-        fileInput("sekwencja", "Choose fasta file.", multiple = FALSE, accept = '.fasta'),
-        numericInput('window_size', 'Width of the read window.', 1000, min = 10, max = 1000000000),
-        numericInput('window_by', 'Width of the window slide.', 10, min = 1, max = 10000),
+                 fileInput("file", label = h3("Upload gene set file")),
+                 selectInput("select", label = h3("Or choose from database"), 
+                             choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+                             selected = 1),
         tabsetPanel(type="tabs",
-                  tabPanel("firstsubpage"),
+                  tabPanel("firstsubpage",
+                           
+                           
+                           ),
                   
-                  tabPanel("secondsubPanel",
+                  tabPanel("Plot$GraphDownload",
                            downloadButton('download_histogram', 'Download png plot'),
                            numericInput('width_hist', 'Plot width [cm]', 20, min = 5, max = 25),
                            numericInput('height_hist', 'Plot height [cm]', 14, min = 5, max = 25),
@@ -31,7 +36,9 @@ ui <- fluidPage(
     
     #głowny panel
         mainPanel(tabsetPanel(type="pills", 
-                          tabPanel("Genes&stuff", ),
+                          tabPanel("Genes&stuff",
+                                   plotOutput("geneplot")
+                                   ),
                           tabPanel("Results summary"),
                           tabPanel("Help&Info")
                                    )
@@ -42,7 +49,20 @@ ui <- fluidPage(
 
 
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  wgranyplik <- reactive({
+    inFile <- input$sekwencja
+    if (is.null(inFile))
+      return(NULL)
+    d <- seqinr::read.fasta(inFile$datapath)
+    return(d)
+  })
+  
+  plotInput <- reactive({
+    p <- geneplot_funtion
+    print(p)
+  })
+}
  
 
 shinyApp(ui = ui, server = server)
